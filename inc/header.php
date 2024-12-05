15270f476193320fb8034fcb8f3bb86e875c3a22
mikiIDE <kinoko.miki@gmail.com>
2024-12-05 13:45:04 +0900

    some changed

---
+++ inc/header.php
@@ -0,0 +1,28 @@
+<?php
+// header.php
+session_start();
+require_once __DIR__ . "/../php/funcs.php";
+// ログイン状態の簡易チェック
+//$is_logged_in = isLoggedIn();
+
+//var_dump($_SESSION);  // デバッグ用＞セッションの中身を確認
+//var_dump($is_logged_in);  // デバッグ用＞ログイン状態を確認
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Kaisei+Decol&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>G's LIFE QUEST</title>
+</head>
+
+<body>
+    <header>
+G's LIFE QUEST
+    </header>
