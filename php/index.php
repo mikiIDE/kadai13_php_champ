111e4441c639310dd9ab0107ba5dca5a18dbdfa2
mikiIDE <kinoko.miki@gmail.com>
2024-12-05 15:10:55 +0900

    sentence changed

--- php/index.php
+++ php/index.php
@@ -5,9 +5,9 @@ require_once __DIR__ . '/../inc/header.php';
 <main>
     <div class="menu-container">
         <p class="intro">モットーは「自分も大事に」</p>
-        <p class="info">日々の睡眠、食事、運動、学習の記録をつけて<br>
-            自分に厳しくしすぎてないか？確認をしましょう</p>
-        <p class="intro">まずは<button class="sign-in"><a href="signin_php">新規登録</a></button></p>
+        <p class="intro">日々の睡眠、食事、運動、学習の記録をつけて<br>
+            自分をよしよし出来てるか？確認をしましょう</p>
+        <p class="intro">まずは<button class="sign-in"><a href="signin.php">新規登録</a></button></p>
         <p class="intro">登録済みの方は以下からログインして進んでください</p>
         <form action="login_act.php" method="post">
             <div class="form-group">
